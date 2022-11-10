2aa3c262489d97a05e534a64d59ac5ee366d59a6
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-09 23:36:27 -0800

    solve easy 07

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,3 @@
-type MyReadonly<T> = any
+/* make use of `keyof` to iterate keys P, looking them up ("index accessed
+types" or "lookup types"), and marking them `readonly` */
+type MyReadonly<T> = { readonly [P in keyof T]: T[P] };
