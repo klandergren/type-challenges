e0ca953e61f78fa578ec67bc45b682d6bcbd1fcd
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-12 20:06:44 -0800

    solve easy 14

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,4 @@
-type First<T extends any[]> = any
+/* remember: `extends` is saying: "can we assign the type on the left to the
+type on the right?". here we want to handle the edge case of an empty array
+returning never: */
+type First<T extends any[]> = T extends [] ? never : T[0];
