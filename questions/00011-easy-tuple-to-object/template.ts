98c50da4eb8f7e1ac8fb26d1b8822c8123a8c5a0
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-12 20:11:45 -0800

    easy 14: clarify that we are mapping the value, not the property

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,5 +1,5 @@
 /* replace `any` with `(string | number)` to restrict tuples containing objects */
 type TupleToObject<T extends readonly (string | number)[]> = {
-  /* use `T[number]` to access every indexed property */
-  [P in T[number]]: P;
+  /* use `T[number]` to access the value every indexed property */
+  [V in T[number]]: V;
 };
