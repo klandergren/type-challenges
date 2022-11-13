514ed1855c2c1a157619f8bffc12c9f0a38b5fb1
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-12 19:10:06 -0800

    solve easy 11

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,5 @@
-type TupleToObject<T extends readonly any[]> = any
+/* replace `any` with `(string | number)` to restrict tuples containing objects */
+type TupleToObject<T extends readonly (string | number)[]> = {
+  /* use `T[number]` to access every indexed property */
+  [P in T[number]]: P;
+};
