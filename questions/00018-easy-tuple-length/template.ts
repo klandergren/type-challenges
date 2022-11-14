e3c0e90c3e5da4a77d886062a4083edae3d574a4
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-14 13:09:27 -0800

    easy 18: solve

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,13 @@
-type Length<T> = any
+/* initially had Length<T extends {length: number}> but saw that there was an
+expected error for Length<'hello world'> so switched to Length<T extends any[]>
+but then got errors on Length<typeof tesla> which was because they were
+readonly types. */
+
+/* the rationale is that `T extends any[]` for `typeof tesla` can be thought of
+as saying "can `typeof tesla` be assignable to `any[]`? and it cannot because
+`readonly` is a narrower form. */
+
+/* getting the resulting number makes use of indexed access types ("lookup
+types") where we know the property we want to look up on the type and return
+its value */
+type Length<T extends readonly any[]> = T["length"];
