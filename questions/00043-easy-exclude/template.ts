7c392adf462e019a9de1d66f09f0e0a726dbb1ab
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-14 14:06:05 -0800

    solve easy 43

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,3 @@
-type MyExclude<T, U> = any
+/* the key here is to understand that conditional types are distributive over
+union types. see https://kip.land/knowledge/typescript for more info */
+type MyExclude<T, U> = T extends U ? never : T;
