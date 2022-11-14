65131aa9669b544af42c024d0b1adeef9566d108
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-14 14:29:41 -0800

    solve easy 189

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,10 @@
-type MyAwaited<T> = any
+/* assuming it is legal to use `PromiseLike`: */
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<any>
+    ? MyAwaited<U>
+    : U
+  : never;
+
+/* note: if there was no error expected on MyAwaited<number> we could get by
+with just: */
+type MyAwaited2<T> = T extends PromiseLike<infer U> ? MyAwaited2<U> : T;
