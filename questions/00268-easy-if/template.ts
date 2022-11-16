0a08fcaaa63ae4dafa5233a5a86d3ae8d8c7b230
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-15 16:14:48 -0800

    solve easy 268

--- questions/00268-easy-if/template.ts
+++ questions/00268-easy-if/template.ts
@@ -1,1 +1,6 @@
-type If<C, T, F> = any
+/* first: add a type constraint via extends to `C` such that it may only be the
+literal types `true` or `false` */
+
+/* second: add a conditional type checking if C can be assigned to `true` and
+if so, returning `T`, otherwise `F` */
+type If<C extends true | false, T, F> = C extends true ? T : F;
