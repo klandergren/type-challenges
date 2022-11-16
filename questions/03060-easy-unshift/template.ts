d8146e65ba8c1ff38c6fbc0e79fc50d21cc3b4fc
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-16 09:01:18 -0800

    solve 3060 easy

--- questions/03060-easy-unshift/template.ts
+++ questions/03060-easy-unshift/template.ts
@@ -1,1 +1,7 @@
-type Unshift<T, U> = any
+/* note: this is the same approach as used in 3057 */
+
+/* first: add type contraints to the generic type variables. prefer using
+`unknown` to ensure downstream type checking */
+
+/* second: use the spread operator to combine the elements of T with U  */
+type Unshift<T extends unknown[], U extends unknown> = [U, ...T];
