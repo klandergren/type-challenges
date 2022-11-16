0898927278bf060269b126190c7c1702f9fdd84d
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-15 17:31:53 -0800

    solve easy 533

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,7 @@
-type Concat<T, U> = any
+/* step 1: add a type constraint to T and U to ensure they are tuple
+types. note: use `unknown[]` rather than `any[]` to ensure that any use is type
+checked */
+
+/* step 2: use the spread operator on each generic type variable to combine
+their elements in a type safe way—called "variadic tuple types". */
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
