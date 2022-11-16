9227e1eb0636a20060667652b2c26680fd190550
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-16 08:59:07 -0800

    solve 3057 easy

--- questions/03057-easy-push/template.ts
+++ questions/03057-easy-push/template.ts
@@ -1,1 +1,5 @@
-type Push<T, U> = any
+/* first: add type contraints to the generic type variables. prefer using
+`unknown` to ensure downstream type checking */
+
+/* second: use the spread operator to combine the elements of T with U  */
+type Push<T extends unknown[], U extends unknown> = [...T, U];
