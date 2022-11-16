dc07e73b84490555f99a69c0ecd58d5d974c0ff2
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-16 09:50:11 -0800

    solve 3312 easy

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,5 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+/* first: prefer conditional type to type constraint, so move `extends` to
+right hand side */
+
+/* second: use `infer` to grab the type of `args` */
+type MyParameters<T> = T extends (...args: infer R) => unknown ? R : never;
