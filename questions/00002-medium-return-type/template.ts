ad78d8bc21f152bb586c498bf3ea6043e6bd1be9
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-17 15:56:41 -0800

    solve 02 return type

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,7 @@
-type MyReturnType<T> = any
+/* step 1: use a conditional type rather than a type constraint as preference
+of returning never rather than generating an error */
+
+/* step 2: write type for any function and infer its return type */
+type MyReturnType<T> = T extends (...args: never[]) => infer ReturnType
+  ? ReturnType
+  : never;
