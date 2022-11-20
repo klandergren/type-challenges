67273015a4717e35e91197f34ac46af4775ee7d8
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-20 08:38:20 -0800

    solve 15 medium

--- questions/00015-medium-last/template.ts
+++ questions/00015-medium-last/template.ts
@@ -1,1 +1,5 @@
-type Last<T extends any[]> = any
+/* step 1: change the type constraint to extends unknown */
+
+/* step 2: use a conditional type and the spread operator to grab the last item
+of the array */
+type Last<T extends unknown[]> = T extends [...any, infer L] ? L : never;
