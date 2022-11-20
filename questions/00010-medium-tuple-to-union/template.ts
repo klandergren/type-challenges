c16f7775574a01c81858463e150bdb87403e8e6d
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-19 17:10:06 -0800

    solve 10 medium

--- questions/00010-medium-tuple-to-union/template.ts
+++ questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,4 @@
-type TupleToUnion<T> = any
+/* note: I think there is some kind of distribution going on here to form the
+union. I do not actually know how TypeScript works behind the scenes to make
+`T[number]` access each value and add it to the union.  */
+type TupleToUnion<T extends unknown[]> = T[number];
