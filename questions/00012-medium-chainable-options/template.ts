bd13fd55054da2d011994ba2f3765c6daa39ed77
Kip Landergren <klandergren@users.noreply.github.com>
2022-11-20 08:38:09 -0800

    solve 12 medium

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,10 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
-}
+/* largely learned from comments at
+https://ghaiklor.github.io/type-challenges-solutions/en/medium-chainable-options.html
+*/
+type Chainable<T = {}> = {
+  option<K extends string, V>(
+    key: K extends keyof T ? (V extends T[K] ? never : K) : K,
+    value: V
+  ): Chainable<Omit<T, K> & { [P in K]: V }>;
+  get(): T;
+};
